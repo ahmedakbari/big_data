ca93c8023c15ed2359c6cbb16759de8fc4daf81a
ahmedakbari <87144544+ahmedakbari@users.noreply.github.com>
2021-07-08 22:18:22 +0430

    Update docker-composer.sh

--- docker-composer.sh
+++ docker-composer.sh
@@ -4,5 +4,5 @@ docker-compose up -d zookeeper kafka redis
 sleep 5
 docker-compose up -d --build twitter_fetcher
 sleep 5
-docker-compose up --build twitter_preprocess
+docker-compose up -d --build twitter_preprocess
 
